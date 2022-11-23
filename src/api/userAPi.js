6d995c561093a565775e2bf6d2f78d4957d42521
unknown <11758926+strart-end@user.noreply.gitee.com>
2022-11-23 12:02:28 +0800

    完成了token续签功能

--- src/api/userAPi.js
+++ src/api/userAPi.js
@@ -1,4 +1,5 @@
 import axios from '@/utils/axios'
+import request from 'axios'
 
 export const getUserInfo = () => {
   return axios.get('/v1_0/user')
@@ -18,4 +19,17 @@ export const updateUserPhoto = (photo) => {
 
 export const updateUserAvatarAPI = (fd) => {
   return axios.patch('/v1_0/user/photo', fd)
+}
+
+// 换取 Token 的 API（形参中的 refreshToken 用来换取新 token）
+export const exchangeTokenAPI = (refreshToken) => {
+  return request({
+    method: 'PUT',
+    // 这里必须填写完整的请求 URL 地址
+    url: 'http://toutiao.itheima.net/v1_0/authorizations',
+    headers: {
+      // 在请求头中携带 Authorization 身份认证字段
+      Authorization: 'Bearer ' + refreshToken,
+    },
+  })
 }
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 import { Toast } from 'vant'
 // 导入vuex模块
 import store from '@/store/index'
+// 按需导入换取 token 的 API
+import { exchangeTokenAPI } from '@/api/userAPi'
 
 const instance = axios.create({
   baseURL: 'http://toutiao.itheima.net',
@@ -35,15 +37,47 @@ instance.interceptors.request.use(
 )
 
 // 响应拦截器（注意：响应拦截器也应该绑定给 instance 实例）
+// 响应拦截器
 instance.interceptors.response.use(
-  // 响应的时候 隐藏 loading 效果
   (response) => {
+    // 隐藏 loading 提示效果
     Toast.clear()
     return response
   },
-  // 响应失败
-  (err) => {
-    return Promise.reject(err)
+  async (error) => {
+    Toast.clear()
+
+    // 1. 从 vuex 中获取 tokenInfo 对象，格式为： { token, refresh_token }
+    const tokenInfo = store.state.tokenInfo
+    // 2. 判断是否为 token 过期
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      tokenInfo.refresh_token
+    ) {
+      // token 过期
+      console.log('token过期啦')
+
+      try {
+        // 3.1 TODO: 发起请求，根据 refresh_token 重新请求一个有效的新 token
+        const { data: res } = await exchangeTokenAPI(tokenInfo.refresh_token)
+        // 3.2 TODO: 更新 Store 中的 Token
+        store.commit('updateTokenInfo', {
+          token: res.data.token,
+          refresh_token: tokenInfo.refresh_token,
+        })
+        // 3.3 重新调用刚才“请求未遂”的接口
+        // 3.3.1 如果在响应拦截器中 return 一个新的 Promise 异步请求，则会把这次请求的结果，当作上次请求的返回值
+        return instance(error.config)
+      } catch {
+        //  token 和 refresh_token 都失效了
+        // 4.1 清空 vuex 和 localStorage
+        store.commit('cleanState')
+        // 4.2 强制跳转到登录页
+        router.replace('/login?pre=' + router.currentRoute.fullPath)
+      }
+    }
+    return Promise.reject(error)
   }
 )
 
